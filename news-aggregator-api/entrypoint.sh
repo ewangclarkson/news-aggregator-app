4f3872e9bc63f223ea592ad22cff20d7e11b2078
clarkson.ewang <clarkson.ewang@gozem.co>
2025-02-18 12:25:53 +0100

    fix: updated docker file to wait for db to be ready before migrations

--- news-aggregator-api/entrypoint.sh
+++ news-aggregator-api/entrypoint.sh
@@ -3,16 +3,21 @@
 # Change to the application directory
 cd /var/www/html/
 
+echo "Waiting for MySQL to be ready..."
+while ! nc -z database 3306; do
+  sleep 0.1 # wait for MySQL to start
+done
+echo "MySQL is ready!"
 
 # Clear caches and run migrations
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate
 
 echo "Running seeders..."
-php artisan db:seed --force
+php artisan db:seed
 
 # Start the Laravel scheduler in the background
 php artisan schedule:run >> /dev/null 2>&1 &
